ceb13b3527647bf0aba511b342051f97ec9686e5
ARTI1208 <leonardo906@mail.ru>
2020-10-26 22:41:34 +0300

    Fixed test were not recognized

--- src/test/java/root/sqrt/AdvSqrtTest.java
+++ src/test/java/root/sqrt/AdvSqrtTest.java
@@ -7,7 +7,18 @@ import static org.testng.Assert.*;
 public class AdvSqrtTest {
 
     @Test
-    void someTest() {
+    void testNonZeroNegative() {
+        AdvSqrt impl = new AdvSqrt();
+
+        double negInfRes = impl.sqrt(Double.NEGATIVE_INFINITY);
+        assertTrue(Double.isNaN(negInfRes));
+
+        double regularNegativeRes = impl.sqrt(-6.67);
+        assertTrue(Double.isNaN(regularNegativeRes));
+    }
+
+    @Test
+    void testNegativeZero() {
 
     }
 }
