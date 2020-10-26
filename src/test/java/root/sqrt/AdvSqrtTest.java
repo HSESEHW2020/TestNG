d8be051faf9291e3759875cfb2894b3de10e07ac
asmaksimov3 <asmaksimov_3@edu.hse.ru>
2020-10-26 22:45:51 +0300

    clear 4 and 5 tasks

--- src/test/java/root/sqrt/AdvSqrtTest.java
+++ src/test/java/root/sqrt/AdvSqrtTest.java
@@ -1,13 +1,29 @@
 package root.sqrt;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
 public class AdvSqrtTest {
 
-    @Test
-    void someTest() {
 
+//    @Test
+//    void roundTest(){
+//        AdvSqrt advSqrt=new AdvSqrt();
+//        Sqrt sqrt=new Sqrt();
+//        Assert.assertEquals(advSqrt.sqrt(33),sqrt.sqrt(33));
+//    }
+    @Test
+    void testNaN(){
+        AdvSqrt advSqrt=new AdvSqrt();
+        Sqrt sqrt=new Sqrt();
+        Assert.assertEquals(advSqrt.sqrt(Double.NaN),Double.NaN);
+    }
+    @Test
+    void testInfinite(){
+        AdvSqrt advSqrt=new AdvSqrt();
+        Sqrt sqrt=new Sqrt();
+        Assert.assertEquals(advSqrt.sqrt(Double.POSITIVE_INFINITY),Double.POSITIVE_INFINITY);
     }
 }
